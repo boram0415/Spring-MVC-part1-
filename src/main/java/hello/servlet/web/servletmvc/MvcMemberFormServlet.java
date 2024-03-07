b6257f2075ffde152ccea75625491b4625c90b4b
boram0415 <boram04415@naver.com>
2024-03-07 19:20:55 +0900

    servlet VS jsp VS MVC

--- src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -14,10 +14,10 @@ import java.io.IOException;
 public class MvcMemberFormServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        // 외보에서 직접적으로 jsp로 접근이 안되게 하기 위해 WEB-INF 경로를 사용 함
         String viewPage = "/WEB-INF/views/new-form.jsp";
         RequestDispatcher dispatcher = req.getRequestDispatcher(viewPage);
-        dispatcher.forward(req,resp);
+          dispatcher.forward(req,resp);
 
     }
 }
